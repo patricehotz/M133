a15a607e9551ff0d4bf9b21f6a38bb888bc30b92
unknown <patrice.hotz@leuchterag.ch>
2022-09-06 19:54:16 +0200

    added changes from step 4

--- Aufgabe.js
+++ Aufgabe.js
@@ -1,8 +1,9 @@
 function Summ(number1, number2, callbackFunction){
+    callbackFunction(number1);
+    callbackFunction(number2);
     callbackFunction(number1 + number2);
 }
+
 function print(number){
     console.log(number)
 }
-
-Summ(3, 4, print)
