ddcc3a188d1cbeea8811529dd03208b88a29e98c
Patrice Hotz <patrice.hotz@leuchterag.ch>
2022-09-14 14:27:33 +0200

    schwanz

--- Aufgabe.js
+++ Aufgabe.js
@@ -1,15 +1,16 @@
-function calculate(resolve, reject){
+function calculateStraightNumbers(resolve, reject){
     let result = 0;
     try{
         for(let i = 0; i < 900000000; i++){
-            result += i;
+            if(!(i % 2)){
+                result += i;
+            }
         }
         resolve(result)
     }
     catch(error){
         reject(error)
     }
-    
 }
 const promise = new Promise(calculate)
 promise.then(result => console.log(result), error => console.error(error))
