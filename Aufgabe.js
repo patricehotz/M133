d87a1ab86e03286ff9d09f4f1bd720a141d00b20
unknown <patrice.hotz@leuchterag.ch>
2022-09-21 13:56:14 +0200

    done Task

--- Aufgabe.js
+++ Aufgabe.js
@@ -1,7 +1,29 @@
-function Summ(number1, number2, callbackFunction){
-    callbackFunction(number1);
-    callbackFunction(number2);
-    callbackFunction(number1+ number2);
+async function calculateEvenNumbers(){
+    let result = 0;
+    for(let i = 0; i < 900000000; i++){
+        if(!(i % 2)){
+            result += i;
+        }
+    }
+    return result
+}
+async function calculateUnevenNumber(){
+    let result = 0;
+
+    for(let i = 0; i < 900000000; i++){
+        if(i % 2){
+            result += i;
+        }
+    }
+    return result
 }
 
-Summ(1 , 2 , number => console.log(number))
+async function asyncCall(){
+    const promiseEvenNumber = calculateEvenNumbers()
+    const promiseUnevenNumer = calculateUnevenNumber()
+
+    console.log(await promiseEvenNumber)
+    console.log(await promiseUnevenNumer)
+}
+
+asyncCall()
